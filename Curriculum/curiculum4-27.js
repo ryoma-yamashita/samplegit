ed1d2e796e008102e9239c2151897fbbaded9d03
ryoma-yamashita <trust.no.justone@outlook.com>
2021-11-21 18:05:05 +0900

    20211121 Curiculum4-28まで学習

---
+++ Curriculum/curiculum4-27.js
@@ -0,0 +1,31 @@
+var variableNumber = 1234;
+
+console.log(variableNumber);
+
+var variableText = "Text";
+
+console.log(variableText);
+
+var variableBoolean = true;
+
+console.log(variableBoolean);
+
+var addNumber = 10 + 20;
+
+console.log(addNumber);
+
+var subtractionNumber = 20 - 10;
+
+console.log(subtractionNumber);
+
+var multiNumber = 50 * 2;
+
+console.log(multiNumber)
+
+var divisionNumber = 20 / 2;
+
+console.log(divisionNumber)
+
+var joinNumber = "12" + "34";
+
+console.log(joinNumber);
---
+++ Curriculum/curiculum4-28.js
@@ -0,0 +1,35 @@
+var boolean = false;
+
+if (boolean) {
+  console.log(boolean);
+};
+
+var num = 100;
+if (num == 100) {
+  console.log(num == 100);
+};
+
+var num = "100";
+if (num == 100) {
+  console.log(num);
+};
+
+var num = "100";
+if (num === 100) {
+  console.log(num);
+};
+
+var variable = true;
+if (variable) {
+  console.log(variable);
+};
+
+var num = 100;
+if (num === 100) {
+  console.log(num);
+};
+
+var text = "JavaScript";
+if (text === "JavaScript") {
+  console.log(text);
+};
